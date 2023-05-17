1a13894b5ee017d48dabdf548f59415fe9862678
Prabesh Thapa <sthapaprabesh2020@gmail.com>
2023-05-17 15:59:31 -0700

    clear all session related cookies

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 ### Auth0 golang
 
-Implementation of google oauth using Auth0
+Implementation of sign in using Google OAuth using Auth0 platform.
+
+
 
 ### Install dependencies
 
 ```
-$ go install
+$ go mod install
 ```
 
 
@@ -13,16 +15,25 @@ $ go install
 
 1. Create an web app in Auth0 portal and fetch values Dashboard > Application > Create application > Select "Regular Web Application" > Create
 
-2. Add callback URL in settings Settings > Allowed callback URls `http://localhost:9090/callback` and Allowed origins as `http://localhost:9090` > Save changes
+2. Add callback URL, Logout URl, and Allowed origins in settings: Application > You Application > Settings
+```
+Callback URL: http://localhost:9090/callback
+Logout URL: http://localhost:9090
+Allowed Origin: http://localhost:9090
+```
+
+Save changes
 
 2. export secrets as environment variables as follows
 
 ```
-export AUTH0_DOMAIN='YOUR VALUE HERE';
-export AUTH0_CLIENT_ID='YOUR VALUE HERE';
-export AUTH0_CLIENT_SECRET='YOUR VALUE HERE';
-export AUTH0_CALLBACK_URL='YOUR VALUE HERE';
+ export AUTH0_DOMAIN='YOUR VALUE HERE';
+ export AUTH0_CLIENT_ID='YOUR VALUE HERE';
+ export AUTH0_CLIENT_SECRET='YOUR VALUE HERE';
+ export AUTH0_CALLBACK_URL='YOUR VALUE HERE';
 ```
+
+Note: If you add a space in front of the shell command, it will not be stored in bash history
 ### Run
 
 ```
--- main.go
+++ main.go
@@ -20,134 +20,24 @@ import (
 	"golang.org/x/oauth2"
 )
 
-type Profile struct {
-	Sub           string `json:"sub"`
-	GivenName     string `json:"given_name"`
-	FamilyName    string `json:"family_name"`
-	Nickname      string `json:"nickname"`
-	Name          string `json:"name"`
-	Picture       string `json:"picture"`
-	Locale        string `json:"locale"`
-	UpdatedAt     string `json:"updated_at"`
-	Email         string `json:"email"`
-	EmailVerified bool   `json:"email_verified"`
+// UserInfo hold infromation fetch using Auth0 resource endpoint
+type UserInfo struct {
+	Sub           string    `json:"sub"`
+	GivenName     string    `json:"given_name"`
+	FamilyName    string    `json:"family_name"`
+	Nickname      string    `json:"nickname"`
+	Name          string    `json:"name"`
+	Picture       string    `json:"picture"`
+	Locale        string    `json:"locale"`
+	UpdatedAt     time.Time `json:"updated_at"`
+	Email         string    `json:"email"`
+	EmailVerified bool      `json:"email_verified"`
 }
 
 // Server represents the HTTP server.
 type Server struct {
 	router       *gin.Engine    // Gin router instance
 	oauth2config *oauth2.Config // OAuth2 configuration
-}
-
-// NewServer creates a new instance of Server.
-func NewServer() (*Server, error) {
-	router := gin.New()
-
-	oauth2Config, err := NewOauth2Config()
-	if err != nil {
-		return nil, fmt.Errorf("could not create new oauth config: %v", err)
-	}
-
-	return &Server{
-		router:       router,
-		oauth2config: oauth2Config,
-	}, nil
-}
-
-// Run starts the server.
-func (s *Server) Run() error {
-	if err := s.router.Run(":9090"); err != nil {
-		return fmt.Errorf("could not start server: %v", err)
-	}
-	return nil
-}
-
-// loginHandler handles the login route.
-func (s *Server) loginHandler(ctx *gin.Context) {
-	state, err := generateRandomString()
-	if err != nil {
-		ctx.String(http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	session := sessions.Default(ctx)
-	session.Set("state", state)
-	if err := session.Save(); err != nil {
-		ctx.String(http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	ctx.Redirect(http.StatusTemporaryRedirect, s.oauth2config.AuthCodeURL(state))
-}
-
-// callbackHandler handles the callback route.
-func (s *Server) callbackHandler(ctx *gin.Context) {
-	session := sessions.Default(ctx)
-	if ctx.Query("state") != session.Get("state") {
-		ctx.String(http.StatusBadRequest, "Invalid state parameter.")
-		return
-	}
-
-	code := ctx.Query("code")
-	token, err := s.oauth2config.Exchange(ctx, code)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, "could not exchange oauth code")
-		return
-	}
-
-	if !token.Valid() {
-		ctx.JSON(http.StatusInternalServerError, "invalid access token")
-		return
-	}
-
-	client := s.oauth2config.Client(context.Background(), token)
-	resp, err := client.Get("https://" + os.Getenv("AUTH0_DOMAIN") + "/userinfo")
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, "invalid access token")
-		return
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	ctx.SetCookie("access_token", token.AccessToken, int(time.Now().Add(1*time.Hour).Unix()), "/", "localhost", true, false)
-	ctx.SetCookie("profile", string(body), int(time.Now().Add(1*time.Hour).Unix()), "/", "localhost", true, false)
-
-	ctx.Redirect(http.StatusTemporaryRedirect, "/profile")
-}
-
-// callbackHandler handles the callback route.
-func (s *Server) logoutHandler(ctx *gin.Context) {
-
-	ctx.SetCookie("access_token", "", int(time.Now().Add(-1*time.Hour).Unix()), "/", "", false, true)
-	ctx.SetCookie("profile", "", int(time.Now().Add(-1*time.Hour).Unix()), "/", "", false, true)
-
-	logoutUrl, err := url.Parse("https://" + os.Getenv("AUTH0_DOMAIN") + "/v2/logout")
-	if err != nil {
-		ctx.String(http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	scheme := "http"
-	if ctx.Request.TLS != nil {
-		scheme = "https"
-	}
-
-	returnTo, err := url.Parse(scheme + "://" + ctx.Request.Host)
-	if err != nil {
-		ctx.String(http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	parameters := url.Values{}
-	parameters.Add("returnTo", returnTo.String())
-	parameters.Add("client_id", os.Getenv("AUTH0_CLIENT_ID"))
-	logoutUrl.RawQuery = parameters.Encode()
-
-	ctx.Redirect(http.StatusTemporaryRedirect, logoutUrl.String())
 }
 
 // NewOauth2Config creates a new OAuth2 configuration.
@@ -159,7 +49,7 @@ func NewOauth2Config() (*oauth2.Config, error) {
 		"https://"+os.Getenv("AUTH0_DOMAIN")+"/",
 	)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not create new provider: %v", err)
 	}
 
 	// Initialize the OAuth2 configuration using the environment variables.
@@ -174,6 +64,205 @@ func NewOauth2Config() (*oauth2.Config, error) {
 	return oauthConfig, nil
 }
 
+// NewServer creates a new instance of Server.
+func NewServer() (*Server, error) {
+	router := gin.New()
+
+	oauth2Config, err := NewOauth2Config()
+	if err != nil {
+		return nil, fmt.Errorf("could not create new oauth config: %v", err)
+	}
+
+	server := &Server{
+		router:       router,
+		oauth2config: oauth2Config,
+	}
+
+	return server, nil
+}
+
+// loginHandler handles the login route.
+func (s *Server) loginHandler(ctx *gin.Context) {
+	state, err := generateRandomString()
+	if err != nil {
+		ctx.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// Save state value in session storage
+	session := sessions.Default(ctx)
+	session.Set("state", state)
+
+	if err := session.Save(); err != nil {
+		ctx.JSON(http.StatusInternalServerError, "could not login")
+		return
+	}
+
+	ctx.Redirect(http.StatusTemporaryRedirect, s.oauth2config.AuthCodeURL(state))
+}
+
+// logoutHandler
+func (s *Server) logoutHandler(ctx *gin.Context) {
+	// delete all the cookies and session values
+	// Set cookie timestamp as negative
+	ctx.SetCookie("at", "", -1, "/", "", false, true)
+	ctx.SetCookie("u", "", -1, "/", "", false, true)
+	ctx.SetCookie("auth-sessions", "", -1, "/", "", false, true)
+
+	// Call auth0 logout endpoint to clear session and tokens from auth0 side
+	logoutURL, err := url.Parse("https://" + os.Getenv("AUTH0_DOMAIN") + "/v2/logout")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, "could not logout")
+		return
+	}
+
+	// Check if request was performed via http or https
+	scheme := "http"
+	if ctx.Request.TLS != nil {
+		scheme = "https"
+	}
+
+	// redirecting user back to homepage
+	redirectionURL, err := url.Parse(scheme + "://" + ctx.Request.Host)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, "could not parse URL")
+		return
+	}
+
+	// add url params
+	parameters := url.Values{}
+	parameters.Add("returnTo", redirectionURL.String())
+	parameters.Add("client_id", os.Getenv("AUTH0_CLIENT_ID"))
+	logoutURL.RawQuery = parameters.Encode()
+
+	ctx.Redirect(http.StatusTemporaryRedirect, logoutURL.String())
+}
+
+// callbackHandler handles the callback route.
+func (s *Server) callbackHandler(ctx *gin.Context) {
+
+	// Checking if state param passed from callback matches what's stored in
+	// memory
+	session := sessions.Default(ctx)
+	if session.Get("state") != ctx.Query("state") {
+		ctx.JSON(http.StatusInternalServerError, "invalid state param")
+		return
+	}
+
+	// get authorization code
+	code := ctx.Query("code")
+	token, err := s.oauth2config.Exchange(ctx, code)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, "could not exchange oauth code")
+		return
+	}
+
+	if !token.Valid() {
+		ctx.JSON(http.StatusInternalServerError, "invalid access token")
+		return
+	}
+
+	// get user information to display in profile
+	client := s.oauth2config.Client(ctx, token)
+	resp, err := client.Get("https://" + os.Getenv("AUTH0_DOMAIN") + "/userinfo")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, "could not fetch user information")
+		return
+	}
+
+	// parse response body
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, "could not parse response body")
+	}
+
+	// TODO: cookie should be encrypted before storing.
+	// save access token and response body in cookie
+	// u => userInfo
+	ctx.SetCookie("u", string(b), int(time.Now().Add(1*time.Hour).Unix()), "/", "localhost", true, true)
+	// at => accessToken
+	ctx.SetCookie("at", token.AccessToken, int(time.Now().Add(1*time.Hour).Unix()), "/", "localhost", true, true)
+
+	ctx.Redirect(http.StatusTemporaryRedirect, "/profile")
+}
+
+func main() {
+	server, err := NewServer()
+	if err != nil {
+		log.Fatalf("could not create new server: %v", err)
+	}
+
+	// Define session storage
+	// TODO: pass this secret from env variable
+	store := cookie.NewStore([]byte("superSecretValue"))
+	server.router.Use(sessions.Sessions("auth-sessions", store))
+
+	server.router.Static("/public", "web/static")
+	server.router.LoadHTMLGlob("web/template/*")
+
+	server.router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "pong")
+	})
+
+	server.router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "home.html", nil)
+	})
+
+	server.router.GET("/profile", IsAuthenticated(), func(ctx *gin.Context) {
+		// Show user information in profile
+
+		userInfo, err := ctx.Cookie("u")
+		if err != nil {
+			// if user info cookie does not exists, then we redirect user back to home page
+			ctx.Redirect(http.StatusTemporaryRedirect, "/")
+			return
+		}
+
+		var u UserInfo
+		if err := json.Unmarshal([]byte(userInfo), &u); err != nil {
+			ctx.JSON(http.StatusInternalServerError, "something wrong. Please try logging in again")
+		}
+
+		ctx.HTML(http.StatusOK, "profile.html", gin.H{
+			"Profile": u,
+		})
+	})
+
+	server.router.GET("/login", server.loginHandler)
+	server.router.GET("/logout", server.logoutHandler)
+
+	server.router.GET("/callback", server.callbackHandler)
+
+	if err := server.router.Run(":9090"); err != nil {
+		log.Fatalf("could not run server: %v", err)
+	}
+}
+
+// Implement authenticaton middleware to make sure user is authenticated before taking to
+// protected endpoint
+func IsAuthenticated() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		accessToken, err := ctx.Cookie("at")
+		if err != nil {
+			// Cookie does not exists, hence abort and redirect user to home page or login page
+			ctx.Redirect(http.StatusTemporaryRedirect, "/")
+			ctx.Abort()
+			return
+		}
+
+		// Check if token is empty
+		if accessToken == "" {
+			// Cookie does not exists, hence abort and redirect user to home page or login page
+			ctx.Redirect(http.StatusTemporaryRedirect, "/")
+			ctx.Abort()
+			return
+		}
+
+		// If everything is okay, forward the request to the handler
+		ctx.Next()
+	}
+}
+
 func generateRandomString() (string, error) {
 	b := make([]byte, 32)
 	_, err := rand.Read(b)
@@ -184,68 +273,4 @@ func generateRandomString() (string, error) {
 	state := base64.StdEncoding.EncodeToString(b)
 
 	return state, nil
-}
-
-func main() {
-	server, err := NewServer()
-	if err != nil {
-		log.Fatalf("could not create new server: %v", err)
-	}
-
-	store := cookie.NewStore([]byte("secret"))
-	server.router.Use(sessions.Sessions("auth-session", store))
-
-	server.router.Static("/public", "web/static")
-	server.router.LoadHTMLGlob("web/template/*")
-
-	server.router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "home.html", nil)
-	})
-
-	server.router.GET("/profile", IsAuthenticated(), func(ctx *gin.Context) {
-
-		profile, err := ctx.Cookie("profile")
-		if err != nil {
-			ctx.Redirect(http.StatusTemporaryRedirect, "/")
-			ctx.Abort()
-			return
-		}
-
-		var p Profile
-		if err := json.Unmarshal([]byte(profile), &p); err != nil {
-			fmt.Println(err)
-		}
-
-		ctx.HTML(http.StatusOK, "profile.html", gin.H{
-			"Profile": p,
-		})
-	})
-
-	server.router.GET("/login", server.loginHandler)
-	server.router.GET("/logout", server.logoutHandler)
-
-	server.router.GET("/callback", server.callbackHandler)
-
-	if err := server.Run(); err != nil {
-		log.Fatalf("could not run server: %v", err)
-	}
-}
-
-func IsAuthenticated() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		accessToken, err := ctx.Cookie("access_token")
-		if err != nil {
-			ctx.Redirect(http.StatusTemporaryRedirect, "/")
-			ctx.Abort()
-			return
-		}
-
-		if accessToken == "" {
-			ctx.Redirect(http.StatusTemporaryRedirect, "/")
-			ctx.Abort()
-			return
-		}
-		ctx.Next()
-
-	}
 }
